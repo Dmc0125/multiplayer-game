b403ab7cbdee8f3a59a1220fd52d753d9462fe1a
dmc0125 <dominikmichal0124@gmail.com>
2026-07-02 09:37:16 +0200

    singleplayer working

---
+++ server/src/game.go
@@ -0,0 +1,312 @@
+package main
+
+import (
+	"encoding/binary"
+	"math"
+	"math/rand"
+
+	"github.com/coder/websocket"
+)
+
+const FRAME_TIME_SECONDS = 1.0 / 60.0
+
+const GAME_WIDTH = 800.0
+const GAME_HEIGHT = 400.0
+
+const PADDLE_WIDTH float32 = 10.0
+const PADDLE_HEIGHT float32 = 100.0
+const PADDLE_LEFT_X = 50.0
+const PADDLE_RIGHT_X = GAME_WIDTH - 50.0 - PADDLE_WIDTH
+const PADDLE_SPEED_PER_SECOND = 200.0
+
+const BALL_RADIUS = 5.0
+const BALL_SPEED_PER_SECOND = 500.0
+
+type GamePaddle struct {
+	x, y float32
+}
+
+type GameBall struct {
+	x, y   float32
+	vx, vy float32
+}
+
+type GamePlayerState struct {
+	conn   *websocket.Conn
+	keys   map[KeyCode]bool
+	left   bool
+	paddle *GamePaddle
+}
+
+type GameStatus uint8
+
+const (
+	GameStatusNone GameStatus = iota
+	GameStatusPlaying
+	GameStatusEnded
+	GameStatusPaused
+)
+
+type GameState struct {
+	players map[ConnId]*GamePlayerState
+	dt      float64
+	status  GameStatus
+	ball    *GameBall
+}
+
+func NewGameState() *GameState {
+	gs := &GameState{
+		players: make(map[ConnId]*GamePlayerState),
+		dt:      0.0,
+		status:  GameStatusNone,
+		ball:    &GameBall{},
+	}
+	return gs
+}
+
+func (gs *GameState) addPlayer(connId ConnId, conn *websocket.Conn) {
+	gs.players[connId] = &GamePlayerState{
+		conn:   conn,
+		keys:   make(map[KeyCode]bool),
+		left:   len(gs.players) == 0,
+		paddle: &GamePaddle{},
+	}
+}
+
+func (gs *GameState) encode() (out []byte) {
+	// connId => 4 bytes, paddleY => 4 bytes = 8 bytes * 2
+	// ballX, ballY 8 bytes
+	out = make([]byte, 16+8)
+	offset := 0
+
+	encodeFloat32 := func(f float32) {
+		binary.LittleEndian.PutUint32(out[offset:], math.Float32bits(f))
+		offset += 4
+	}
+
+	for connId, p := range gs.players {
+		binary.LittleEndian.PutUint32(out[offset:], uint32(connId))
+		offset += 4
+		encodeFloat32(p.paddle.y)
+	}
+
+	encodeFloat32(gs.ball.x)
+	encodeFloat32(gs.ball.y)
+	return
+}
+
+func (gs *GameState) start() {
+	gs.status = GameStatusPlaying
+	gs.dt = 0
+
+	// paddles
+	for _, p := range gs.players {
+		if p.left {
+			p.paddle.x = PADDLE_LEFT_X
+		} else {
+			p.paddle.x = PADDLE_RIGHT_X
+		}
+		p.paddle.y = GAME_HEIGHT/2 - PADDLE_HEIGHT/2
+	}
+
+	// ball
+	gs.ball.x = GAME_WIDTH / 2
+	gs.ball.y = GAME_HEIGHT / 2
+
+	{ // ball velocity
+		deg := rand.Float32()*30 - 15 // [-15, 15]
+		rad := float64(deg * math.Pi / 180)
+
+		gs.ball.vx = float32(math.Cos(rad)) * BALL_SPEED_PER_SECOND
+		gs.ball.vy = float32(math.Sin(rad)) * BALL_SPEED_PER_SECOND
+
+		if rand.Float32() < 0.5 {
+			gs.ball.vx = -gs.ball.vx
+		}
+	}
+}
+
+func predictBallY(ball GameBall, paddleLeft bool) (predictedY float32, ok bool) {
+	if ball.x < PADDLE_LEFT_X || ball.x > PADDLE_RIGHT_X {
+		return
+	}
+
+	updateBall := func(b *GameBall) {
+		b.y += b.vy * float32(FRAME_TIME_SECONDS)
+		b.x += b.vx * float32(FRAME_TIME_SECONDS)
+
+		if b.y-BALL_RADIUS < 0 {
+			b.y = BALL_RADIUS
+			b.vy = -b.vy
+		} else if b.y+BALL_RADIUS > GAME_HEIGHT {
+			b.y = GAME_HEIGHT - BALL_RADIUS
+			b.vy = -b.vy
+		}
+	}
+
+	if paddleLeft && ball.vx < 0 {
+		// ball going left
+		ok = true
+		for {
+			updateBall(&ball)
+			if ball.x-BALL_RADIUS < PADDLE_LEFT_X+PADDLE_WIDTH {
+				predictedY = ball.y
+				break
+			}
+		}
+	} else if !paddleLeft && ball.vx > 0 {
+		// ball going left
+		ok = true
+		for {
+			updateBall(&ball)
+			if ball.x+BALL_RADIUS > PADDLE_RIGHT_X {
+				predictedY = ball.y
+				break
+			}
+		}
+	}
+
+	return
+}
+
+func (gs *GameState) update() (winner bool, connId ConnId) {
+	dt := float32(gs.dt)
+
+	for _, p := range gs.players {
+		if p.conn == nil {
+			predictedY, ok := predictBallY(*gs.ball, p.left)
+			const deadzone = 10.0
+			if ok {
+				if predictedY > p.paddle.y+PADDLE_HEIGHT-deadzone {
+					p.keys[KeyCodeArrowUp] = true
+				} else if predictedY < p.paddle.y+deadzone {
+					p.keys[KeyCodeArrowDown] = true
+				}
+			}
+		}
+
+		// update paddle
+		paddle := p.paddle
+		if p.keys[KeyCodeArrowUp] {
+			paddle.y += PADDLE_SPEED_PER_SECOND * dt
+			if paddle.y+PADDLE_HEIGHT > GAME_HEIGHT {
+				paddle.y = GAME_HEIGHT - PADDLE_HEIGHT
+			}
+		}
+		if p.keys[KeyCodeArrowDown] {
+			paddle.y -= PADDLE_SPEED_PER_SECOND * dt
+			if paddle.y < 0 {
+				paddle.y = 0
+			}
+		}
+
+		if p.conn == nil {
+			p.keys[KeyCodeArrowUp] = false
+			p.keys[KeyCodeArrowDown] = false
+		}
+	}
+
+	{ // ball
+		ball := gs.ball
+		ball.x += ball.vx * dt
+		ball.y += ball.vy * dt
+
+		setWinner := func(left bool) {
+			winner = true
+			for c, p := range gs.players {
+				if left && p.left {
+					connId = c
+					return
+				} else if !left && !p.left {
+					connId = c
+					return
+				}
+			}
+		}
+
+		switch {
+		case ball.x-BALL_RADIUS < 0:
+			// player right wins
+			gs.status = GameStatusEnded
+			setWinner(false)
+			return
+		case ball.x+BALL_RADIUS > GAME_WIDTH:
+			// player left wins
+			gs.status = GameStatusEnded
+			setWinner(true)
+			return
+		case ball.y-BALL_RADIUS < 0:
+			ball.y = BALL_RADIUS
+			ball.vy = -ball.vy
+			return
+		case ball.y+BALL_RADIUS > GAME_HEIGHT:
+			ball.y = GAME_HEIGHT - BALL_RADIUS
+			ball.vy = -ball.vy
+			return
+		}
+
+		// ball collision with paddles
+		for _, p := range gs.players {
+			ballLeft := ball.x - BALL_RADIUS
+			ballRight := ball.x + BALL_RADIUS
+			ballTop := ball.y + BALL_RADIUS
+			ballBottom := ball.y - BALL_RADIUS
+
+			paddleLeft := p.paddle.x
+			paddleRight := p.paddle.x + PADDLE_WIDTH
+			paddleTop := p.paddle.y + PADDLE_HEIGHT
+			paddleBottom := p.paddle.y
+
+			xInside := ballRight > paddleLeft && ballLeft < paddleRight
+			yInside := ballTop > paddleBottom && ballBottom < paddleTop
+
+			if !(xInside && yInside) {
+				continue
+			}
+
+			overlapLeft := ballRight - paddleLeft
+			overlapRight := paddleRight - ballLeft
+			overlapTop := paddleTop - ballBottom
+			overlapBottom := ballTop - paddleBottom
+
+			xMin := float32(math.Min(float64(overlapLeft), float64(overlapRight)))
+			yMin := float32(math.Min(float64(overlapTop), float64(overlapBottom)))
+
+			bounceAngle := func(vxDir float32) (vx, vy float32) {
+				paddleHalfHeight := PADDLE_HEIGHT / 2
+				paddleCenter := p.paddle.y + paddleHalfHeight
+				hit := ball.y - paddleCenter
+
+				rel := float32(math.Max(-1, math.Min(1, float64(hit/paddleHalfHeight))))
+				angle := 55 * rel
+
+				rad := float64(angle * math.Pi / 180)
+				vx = float32(math.Cos(rad)) * BALL_SPEED_PER_SECOND * vxDir
+				vy = float32(math.Sin(rad)) * BALL_SPEED_PER_SECOND
+
+				return
+			}
+
+			if xMin < yMin {
+				if overlapLeft < overlapRight {
+					ball.x = paddleLeft - BALL_RADIUS
+					ball.vx, ball.vy = bounceAngle(-1)
+				} else {
+					ball.x = paddleRight + BALL_RADIUS
+					ball.vx, ball.vy = bounceAngle(1)
+				}
+			} else {
+				if overlapTop < overlapBottom {
+					ball.y = paddleTop + BALL_RADIUS
+				} else {
+					ball.y = paddleBottom - BALL_RADIUS
+				}
+				ball.vy = -ball.vy
+			}
+
+			break
+		}
+	}
+
+	return
+}
--- server/src/main.go
+++ server/src/main.go
@@ -9,8 +9,6 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"math"
-	"math/rand"
 	"net/http"
 	"os"
 	"time"
@@ -23,527 +21,6 @@ import (
 )
 
 const PORT = ":8080"
-
-type MessageType uint8
-
-const (
-	MessageTypeConnId MessageType = iota
-	MessageTypeGameStart
-	MessageTypeGameRestart
-	MessageTypeGameEnd
-	MessageTypeGameState
-	MessageTypeKey
-	MessageTypeLeave
-)
-
-type ConnId uint32
-
-var nextConnId = func() func() ConnId {
-	c := 0
-	return func() ConnId {
-		c++
-		return ConnId(c)
-	}
-}()
-
-type KeyCode uint8
-
-const (
-	KeyCodeArrowUp KeyCode = iota
-	KeyCodeArrowDown
-)
-
-type GameInput struct {
-	connId  ConnId
-	keyCode KeyCode
-	pressed bool
-}
-
-type JoinMessage struct {
-	ConnId       ConnId
-	Conn         *websocket.Conn
-	Cancel       context.CancelFunc
-	Singleplayer bool
-}
-
-type GameLoop struct {
-	joinChan    chan JoinMessage
-	leaveChan   chan ConnId
-	inputChan   chan GameInput
-	restartChan chan ConnId
-}
-
-const (
-	FRAME_TIME_SECONDS float64 = 1.0 / 60.0
-	GAME_WIDTH                 = 800.0
-	GAME_HEIGHT                = 400.0
-
-	PADDLE_SPEED_PER_SECOND = 200.0
-	PADDLE_WIDTH            = 10.0
-	PADDLE_HEIGHT           = 100.0
-
-	PADDLE_LEFT_X  = 50.0
-	PADDLE_RIGHT_X = GAME_WIDTH - 50.0
-
-	BALL_SPEED_PER_SECOND = 500.0
-	BALL_RADIUS           = 5.0
-)
-
-type Paddle struct {
-	width, height float32
-	x, y          float32
-}
-
-type Ball struct {
-	radius float32
-	x, y   float32
-	vx, vy float32
-}
-
-type PlayerState struct {
-	left       bool
-	connection *websocket.Conn
-	keys       map[KeyCode]bool
-	paddle     Paddle
-	bot        bool
-	restart    bool
-}
-
-func (gl *GameLoop) run() {
-	leftMissing := true
-	players := make(map[ConnId]*PlayerState)
-	singleplayer := false
-	var ball Ball
-
-	startTime := time.Now()
-	gameRunning := false
-	elapsedTime := 0.0
-	deltaTime := 0.0
-
-	join := func(connId ConnId, bot bool, conn *websocket.Conn) {
-		ps := &PlayerState{
-			connection: conn,
-			keys:       make(map[KeyCode]bool),
-			bot:        bot,
-		}
-
-		var paddleX float32
-		if leftMissing {
-			ps.left = true
-			leftMissing = false
-			paddleX = 50
-		} else {
-			ps.left = false
-			paddleX = GAME_WIDTH - 50 - PADDLE_WIDTH
-		}
-
-		ps.paddle = Paddle{
-			width:  PADDLE_WIDTH,
-			height: PADDLE_HEIGHT,
-			x:      paddleX,
-			y:      GAME_HEIGHT/2 - PADDLE_HEIGHT/2,
-		}
-		players[connId] = ps
-	}
-
-	leave := func(connId ConnId) {
-		if players[connId].left {
-			leftMissing = true
-		}
-		delete(players, connId)
-	}
-
-	broadcast := func(msgType MessageType, data []byte) {
-		d := []byte{byte(msgType)}
-		if data != nil {
-			d = append(d, data...)
-		}
-
-		for _, p := range players {
-			if p.connection != nil {
-				p.connection.Write(context.Background(), websocket.MessageBinary, d)
-			}
-		}
-	}
-
-	stateEncode := func() []byte {
-		encoded := make([]byte, 40+12)
-		offset := 0
-
-		encodef32 := func(f float32) {
-			binary.LittleEndian.PutUint32(encoded[offset:], math.Float32bits(f))
-			offset += 4
-		}
-
-		for connId, p := range players {
-			binary.LittleEndian.PutUint32(encoded[offset:], uint32(connId))
-			offset += 4
-			encodef32(p.paddle.x)
-			encodef32(p.paddle.y)
-			encodef32(p.paddle.width)
-			encodef32(p.paddle.height)
-		}
-
-		encodef32(ball.x)
-		encodef32(ball.y)
-		encodef32(ball.radius)
-
-		return encoded
-	}
-
-	predictBallY := func(ball Ball, paddle Paddle) (float32, bool) {
-		if ball.x < PADDLE_LEFT_X || ball.x > PADDLE_RIGHT_X {
-			return 0, false
-		}
-
-		updateBall := func(b *Ball) {
-			b.y += b.vy * float32(FRAME_TIME_SECONDS)
-			b.x += b.vx * float32(FRAME_TIME_SECONDS)
-
-			if b.y-b.radius < 0 {
-				b.y = b.radius
-				b.vy = -b.vy
-			} else if b.y+b.radius > GAME_HEIGHT {
-				b.y = GAME_HEIGHT - b.radius
-				b.vy = -b.vy
-			}
-		}
-
-		if paddle.x == PADDLE_LEFT_X && ball.vx < 0 { // left paddle
-			for {
-				updateBall(&ball)
-				if ball.x-ball.radius < paddle.x+paddle.width {
-					return ball.y, true
-				}
-			}
-		} else if ball.vx > 0 { // right paddle
-			for {
-				updateBall(&ball)
-				if ball.x+ball.radius > paddle.x {
-					return ball.y, true
-				}
-			}
-		}
-
-		return 0, false
-	}
-
-	startGame := func() {
-		gameRunning = true
-		elapsedTime = 0.0
-		deltaTime = 0.0
-
-		// paddles
-		for _, p := range players {
-			p.paddle.y = GAME_HEIGHT/2 - p.paddle.height/2
-		}
-
-		// ball
-		ball.radius = BALL_RADIUS
-		ball.x = GAME_WIDTH/2 - ball.radius/2
-		ball.y = GAME_HEIGHT/2 - ball.radius/2
-
-		{ // ball velocity
-			deg := rand.Float32()*30 - 15 // [-15, 15]
-			rad := float64(deg * math.Pi / 180)
-
-			ball.vx = float32(math.Cos(rad)) * BALL_SPEED_PER_SECOND
-			ball.vy = float32(math.Sin(rad)) * BALL_SPEED_PER_SECOND
-
-			if rand.Float32() < 0.5 {
-				ball.vx = -ball.vx
-			}
-		}
-
-		broadcast(MessageTypeGameStart, stateEncode())
-	}
-
-	for {
-		select {
-		case j := <-gl.joinChan:
-			if len(players) == 2 {
-				j.Cancel()
-				j.Conn.Close(websocket.StatusNormalClosure, "Error: game already running")
-			} else {
-				join(j.ConnId, false, j.Conn)
-
-				if len(players) == 1 && j.Singleplayer {
-					join(nextConnId(), true, nil)
-					singleplayer = true
-					startGame()
-				}
-
-				if len(players) == 2 {
-					startGame()
-				}
-			}
-		case connId := <-gl.leaveChan:
-			gameRunning = false
-			if singleplayer {
-				for c, p := range players {
-					if p.bot {
-						leave(c)
-						break
-					}
-				}
-			} else {
-				broadcast(MessageTypeLeave, nil)
-			}
-
-			leave(connId)
-		case inp := <-gl.inputChan:
-			p := players[inp.connId]
-			p.keys[inp.keyCode] = inp.pressed
-		case connId := <-gl.restartChan:
-			if !gameRunning {
-				if singleplayer {
-					startGame()
-				} else {
-					players[connId].restart = true
-					playersReady := true
-					for _, p := range players {
-						if !p.restart {
-							playersReady = false
-							break
-						}
-					}
-
-					if playersReady {
-						startGame()
-						for _, p := range players {
-							p.restart = false
-						}
-					}
-				}
-			}
-		default:
-			{ // update
-				dt := float32(deltaTime)
-
-				if gameRunning {
-					// paddles
-					for _, p := range players {
-						func() {
-							if p.bot {
-								if predictedY, ok := predictBallY(ball, p.paddle); ok {
-									if predictedY > p.paddle.y+p.paddle.height {
-										p.keys[KeyCodeArrowUp] = true
-									} else if predictedY < p.paddle.y {
-										p.keys[KeyCodeArrowDown] = true
-									}
-								}
-							}
-							defer func() {
-								if p.bot {
-									p.keys[KeyCodeArrowUp] = false
-									p.keys[KeyCodeArrowDown] = false
-								}
-							}()
-
-							//
-
-							paddle := &p.paddle
-
-							if p.keys[KeyCodeArrowUp] {
-								paddle.y += PADDLE_SPEED_PER_SECOND * dt
-
-								if paddle.y+paddle.height > GAME_HEIGHT {
-									paddle.y = GAME_HEIGHT - paddle.height
-								}
-							}
-							if p.keys[KeyCodeArrowDown] {
-								paddle.y -= PADDLE_SPEED_PER_SECOND * dt
-
-								if paddle.y < 0 {
-									paddle.y = 0
-								}
-							}
-						}()
-					}
-
-					func() {
-						sendState := true
-						defer func() {
-							if sendState {
-								broadcast(MessageTypeGameState, stateEncode())
-							}
-						}()
-
-						// ball
-						ball.x += ball.vx * dt
-						ball.y += ball.vy * dt
-
-						switch {
-						case ball.x-ball.radius < 0:
-							// player right wins
-							sendState = false
-							broadcast(MessageTypeGameEnd, []byte{1})
-							gameRunning = false
-							return
-						case ball.x+ball.radius > GAME_WIDTH:
-							// player left wins
-							sendState = false
-							broadcast(MessageTypeGameEnd, []byte{0})
-							gameRunning = false
-							return
-						case ball.y-ball.radius < 0:
-							ball.y = ball.radius
-							ball.vy = -ball.vy
-							return
-						case ball.y+ball.radius > GAME_HEIGHT:
-							ball.y = GAME_HEIGHT - ball.radius
-							ball.vy = -ball.vy
-							return
-						}
-
-						// ball collision with paddles
-
-						for _, p := range players {
-							ballLeft := ball.x - ball.radius
-							ballRight := ball.x + ball.radius
-							ballTop := ball.y + ball.radius
-							ballBottom := ball.y - ball.radius
-
-							paddleLeft := p.paddle.x
-							paddleRight := p.paddle.x + p.paddle.width
-							paddleTop := p.paddle.y + p.paddle.height
-							paddleBottom := p.paddle.y
-
-							xInside := ballRight > paddleLeft && ballLeft < paddleRight
-							yInside := ballTop > paddleBottom && ballBottom < paddleTop
-
-							if !(xInside && yInside) {
-								continue
-							}
-
-							overlapLeft := ballRight - paddleLeft
-							overlapRight := paddleRight - ballLeft
-							overlapTop := paddleTop - ballBottom
-							overlapBottom := ballTop - paddleBottom
-
-							xMin := float32(math.Min(float64(overlapLeft), float64(overlapRight)))
-							yMin := float32(math.Min(float64(overlapTop), float64(overlapBottom)))
-
-							bounceAngle := func(vxDir float32) (vx, vy float32) {
-								paddleHalfHeight := p.paddle.height / 2
-								paddleCenter := p.paddle.y + paddleHalfHeight
-								hit := ball.y - paddleCenter
-
-								rel := float32(math.Max(-1, math.Min(1, float64(hit/paddleHalfHeight))))
-								angle := 55 * rel
-
-								rad := float64(angle * math.Pi / 180)
-								vx = float32(math.Cos(rad)) * BALL_SPEED_PER_SECOND * vxDir
-								vy = float32(math.Sin(rad)) * BALL_SPEED_PER_SECOND
-
-								return
-							}
-
-							if xMin < yMin {
-								if overlapLeft < overlapRight {
-									ball.x = paddleLeft - ball.radius
-									ball.vx, ball.vy = bounceAngle(-1)
-								} else {
-									ball.x = paddleRight + ball.radius
-									ball.vx, ball.vy = bounceAngle(1)
-								}
-							} else {
-								if overlapTop < overlapBottom {
-									ball.y = paddleTop + ball.radius
-								} else {
-									ball.y = paddleBottom - ball.radius
-								}
-								ball.vy = -ball.vy
-							}
-
-							break
-						}
-					}()
-				}
-			}
-
-			{ // time
-				dt := time.Since(startTime).Seconds()
-				if dt < FRAME_TIME_SECONDS {
-					diff := FRAME_TIME_SECONDS - dt
-					time.Sleep(time.Duration(diff * 1e9))
-					dt += diff
-				}
-
-				// set time
-				if gameRunning {
-					elapsedTime += dt
-					deltaTime = dt
-				}
-
-				startTime = time.Now()
-			}
-		}
-	}
-}
-
-func wsHandler(gl *GameLoop) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
-			InsecureSkipVerify: true,
-		})
-		if err != nil {
-			log.Printf("Error: unable to accept websocket connection: %s", err)
-			http.Error(w, "Error: unable to accept websocket connection", http.StatusInternalServerError)
-			return
-		}
-		defer conn.Close(websocket.StatusNormalClosure, "")
-
-		connId := nextConnId()
-		{ // send conn id
-			d := []byte{byte(MessageTypeConnId)}
-			d = binary.LittleEndian.AppendUint32(d, uint32(connId))
-
-			ctx, _ := context.WithTimeout(context.Background(), time.Second)
-			if err := conn.Write(ctx, websocket.MessageBinary, d); err != nil {
-				log.Printf("Error: unable to send conn id: %s", err)
-				return
-			}
-		}
-
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		for {
-			msgType, bytes, err := conn.Read(ctx)
-			if errors.Is(err, context.Canceled) {
-				return
-			} else if err != nil {
-				gl.leaveChan <- connId
-				return
-			}
-
-			if msgType != websocket.MessageBinary {
-				log.Printf("Error: unexpected message type: %d\n", msgType)
-				continue
-			}
-			if len(bytes) < 1 {
-				log.Printf("Error: invalid message length: %d\n", len(bytes))
-				continue
-			}
-
-			m := MessageType(bytes[0])
-
-			switch m {
-			case MessageTypeGameStart:
-				singleplayer := bytes[1] == 0
-				gl.joinChan <- JoinMessage{connId, conn, cancel, singleplayer}
-			case MessageTypeGameRestart:
-				gl.restartChan <- connId
-			case MessageTypeKey:
-				gl.inputChan <- GameInput{connId, KeyCode(bytes[1]), bytes[2] == 1}
-			case MessageTypeLeave:
-				gl.leaveChan <- connId
-				return
-			}
-		}
-	}
-}
 
 // auth
 
@@ -669,7 +146,7 @@ func callbackHandler(dbConn *pgx.Conn) func(w http.ResponseWriter, r *http.Reque
 			Secure:   isProd,
 			SameSite: http.SameSiteLaxMode,
 		})
-		http.Redirect(w, r, os.Getenv("CLIENT_REDIRECT_URL"), http.StatusPermanentRedirect)
+		http.Redirect(w, r, os.Getenv("CLIENT_REDIRECT_URL"), http.StatusSeeOther)
 	}
 }
 
@@ -702,7 +179,281 @@ func authLogoutHandler(dbConn *pgx.Conn) func(w http.ResponseWriter, r *http.Req
 			Secure:   false,
 			SameSite: http.SameSiteLaxMode,
 		})
-		http.Redirect(w, r, os.Getenv("CLIENT_REDIRECT_URL"), http.StatusPermanentRedirect)
+		http.Redirect(w, r, os.Getenv("CLIENT_REDIRECT_URL"), http.StatusSeeOther)
+	}
+}
+
+// game
+
+func authenticate(dbConn *pgx.Conn, w http.ResponseWriter, r *http.Request) (ok bool) {
+	sessionId, err := r.Cookie("session_id")
+	if err != nil {
+		log.Printf("Error: unable to get session id: %s", err)
+		http.Error(w, "Error: unable to get session id", http.StatusInternalServerError)
+		return
+	}
+	if sessionId == nil {
+		http.Error(w, "Error: session id not found", http.StatusUnauthorized)
+		return
+	}
+	err = dbConn.QueryRow(r.Context(), "select true from sessions where id = $1", sessionId.Value).Scan(&ok)
+	if errors.Is(err, pgx.ErrNoRows) {
+		http.Error(w, "Error: session not found", http.StatusUnauthorized)
+	} else if err != nil {
+		log.Printf("Error: unable to get session: %s", err)
+		http.Error(w, "Error: unable to get session", http.StatusInternalServerError)
+	}
+	return
+}
+
+type ConnId uint32
+
+var nextConnId = func() func() ConnId {
+	c := 0
+	return func() ConnId {
+		c++
+		return ConnId(c)
+	}
+}()
+
+type MessageType uint8
+
+const (
+	// server -> client
+	MessageTypeJoined MessageType = iota
+	MessageTypeFull
+	MessageTypeStarted
+	MessageTypeGameState
+	MessageTypeGameEnd
+
+	// client -> server
+	__MessageTypeClientToServer
+	MessageTypeKey = 100 + iota - __MessageTypeClientToServer - 1
+	MessageTypeStart
+)
+
+type LobbyStatus uint8
+
+const (
+	LobbyStatusWaitingForPlayer LobbyStatus = iota
+)
+
+type KeyCode uint8
+
+const (
+	KeyCodeArrowUp KeyCode = iota
+	KeyCodeArrowDown
+	KeyCodeSpace
+)
+
+type PlayerState struct {
+	conn *websocket.Conn
+	keys map[KeyCode]bool
+}
+
+type LobbyMessage struct {
+	connId  ConnId
+	msgType MessageType
+	data    []byte
+}
+
+type GameLobby struct {
+	singleplayer bool
+	messageChan  chan LobbyMessage
+	stopChan     chan struct{}
+
+	//
+	game *GameState
+}
+
+func NewSingleplayerGameLobby(connId ConnId, conn *websocket.Conn) (gl *GameLobby) {
+	gl = &GameLobby{
+		singleplayer: true,
+		messageChan:  make(chan LobbyMessage),
+		stopChan:     make(chan struct{}),
+		game:         NewGameState(),
+	}
+	gl.game.addPlayer(connId, conn)
+	gl.game.addPlayer(nextConnId(), nil)
+	return
+}
+
+func NewMultiplayerGameLobby() (gl *GameLobby) {
+	gl = &GameLobby{
+		singleplayer: false,
+		messageChan:  make(chan LobbyMessage),
+		stopChan:     make(chan struct{}),
+		game:         NewGameState(),
+	}
+	return
+}
+
+func (gl *GameLobby) broadcast(msgType MessageType, data []byte) {
+	d := []byte{byte(msgType)}
+	if data != nil {
+		d = append(d, data...)
+	}
+
+	for _, p := range gl.game.players {
+		if p.conn != nil {
+			p.conn.Write(context.Background(), websocket.MessageBinary, d)
+		}
+	}
+}
+
+func (gl *GameLobby) start() {
+	startTime := time.Now()
+
+	for {
+		select {
+		case <-gl.stopChan:
+			return
+		case lm := <-gl.messageChan:
+			switch lm.msgType {
+			case MessageTypeKey:
+				if len(lm.data) < 2 {
+					log.Printf("Error: invalid message length: %d\n", len(lm.data))
+					continue
+				}
+
+				keyCode := KeyCode(lm.data[0])
+				pressed := lm.data[1] == 1
+
+				// TODO: needs to be handled for each player
+				if pressed && keyCode == KeyCodeSpace {
+					switch gl.game.status {
+					case GameStatusPlaying:
+						gl.game.status = GameStatusPaused
+					case GameStatusPaused:
+						gl.game.status = GameStatusPlaying
+					}
+				}
+
+				gl.game.players[lm.connId].keys[keyCode] = pressed
+			case MessageTypeStart:
+				if gl.game.status == GameStatusEnded || gl.game.status == GameStatusNone {
+					// TODO: both players ready
+					gl.game.start()
+					gl.broadcast(MessageTypeStarted, gl.game.encode())
+				}
+			}
+		default:
+			// update
+
+			if gl.game.status == GameStatusPlaying {
+				winner, connId := gl.game.update()
+				if winner {
+					d := []byte{}
+					d = binary.LittleEndian.AppendUint32(d, uint32(connId))
+					gl.broadcast(MessageTypeGameEnd, d)
+				} else {
+					gl.broadcast(MessageTypeGameState, gl.game.encode())
+				}
+			}
+
+			// time
+
+			dt := time.Since(startTime).Seconds()
+			if dt < FRAME_TIME_SECONDS {
+				diff := FRAME_TIME_SECONDS - dt
+				time.Sleep(time.Duration(diff * 1e9))
+				dt += diff
+			}
+
+			if gl.game.status == GameStatusPlaying {
+				gl.game.dt = dt
+			}
+
+			startTime = time.Now()
+		}
+	}
+}
+
+func gameHandler(dbConn *pgx.Conn) func(w http.ResponseWriter, r *http.Request) {
+	var lobbies [10]*GameLobby
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !authenticate(dbConn, w, r) {
+			return
+		}
+
+		singleplayer := r.URL.Query().Get("singleplayer") == "1"
+
+		conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
+			InsecureSkipVerify: true,
+		})
+		if err != nil {
+			log.Printf("Error: unable to accept websocket connection: %s", err)
+			http.Error(w, "Error: unable to accept websocket connection", http.StatusInternalServerError)
+			return
+		}
+
+		connId := nextConnId()
+		var lobbyIdx int
+		var lobby *GameLobby
+
+		if singleplayer {
+			freeIdx := -1
+			for idx, lobby := range lobbies {
+				if lobby == nil {
+					freeIdx = idx
+					break
+				}
+			}
+
+			if freeIdx == -1 {
+				if err := conn.Write(r.Context(), websocket.MessageBinary, []byte{byte(MessageTypeFull)}); err != nil {
+					log.Printf("Error: unable to send message: %s", err)
+					return
+				}
+			} else {
+				lobby = NewSingleplayerGameLobby(connId, conn)
+				lobbyIdx = freeIdx
+				lobbies[freeIdx] = lobby
+
+				d := []byte{byte(MessageTypeJoined)}
+				d = binary.LittleEndian.AppendUint32(d, uint32(connId))
+				d = append(d, uint8(0))
+				if err := conn.Write(r.Context(), websocket.MessageBinary, d); err != nil {
+					log.Printf("Error: unable to send message: %s", err)
+					return
+				}
+
+				go lobby.start()
+			}
+		} else {
+			for idx, lobby := range lobbies {
+
+			}
+		}
+
+		defer func() {
+			if lobby.singleplayer {
+				lobby.stopChan <- struct{}{}
+				lobbies[lobbyIdx] = nil
+			}
+		}()
+
+		for {
+			msgType, bytes, err := conn.Read(r.Context())
+			if err != nil {
+				return
+			}
+
+			if msgType != websocket.MessageBinary {
+				log.Printf("Error: unexpected message type: %d\n", msgType)
+				continue
+			}
+			if len(bytes) < 1 {
+				log.Printf("Error: invalid message length: %d\n", len(bytes))
+				continue
+			}
+
+			m := MessageType(bytes[0])
+			if lobby != nil {
+				lobby.messageChan <- LobbyMessage{connId, m, bytes[1:]}
+			}
+		}
 	}
 }
 
@@ -738,15 +489,7 @@ func main() {
 	http.HandleFunc("/auth", authHandler)
 	http.HandleFunc("/callback", callbackHandler(dbConn))
 	http.HandleFunc("/logout", authLogoutHandler(dbConn))
-
-	game := GameLoop{
-		joinChan:    make(chan JoinMessage),
-		leaveChan:   make(chan ConnId),
-		inputChan:   make(chan GameInput),
-		restartChan: make(chan ConnId),
-	}
-	go game.run()
-	http.HandleFunc("/ws", wsHandler(&game))
+	http.HandleFunc("/game", gameHandler(dbConn))
 
 	log.Printf("Listening at http://localhost%s\n", PORT)
 	if err := http.ListenAndServe(PORT, nil); err != nil {
